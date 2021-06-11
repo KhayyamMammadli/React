49710886cc35106d767306c1acad48b4a1773924
khayyam mammadli <khayyammammadli@khayyams-mbp.localdomain>
2021-06-11 14:51:56 +0400

    chnage

---
+++ src/inc/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <div><hr/>Footer</div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
